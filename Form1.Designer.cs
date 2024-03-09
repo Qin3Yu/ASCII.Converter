81bf850a122f5787726f05368285c9dfebc46ac1
Qin3Yu <wrytinter@yeah.net>
2024-03-09 21:29:06 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace ASCII.Converter {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label_Char = new System.Windows.Forms.Label();
+            this.label_ASCII = new System.Windows.Forms.Label();
+            this.textBox_Char1 = new System.Windows.Forms.TextBox();
+            this.textBox_ASCII1 = new System.Windows.Forms.TextBox();
+            this.label_PS1 = new System.Windows.Forms.Label();
+            this.textBox_Char2 = new System.Windows.Forms.TextBox();
+            this.textBox_ASCII2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_ASCII3 = new System.Windows.Forms.TextBox();
+            this.textBox_Char3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button_Converter = new System.Windows.Forms.Button();
+            this.button_Clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_Char
+            // 
+            this.label_Char.AutoSize = true;
+            this.label_Char.Font = new System.Drawing.Font("Consolas", 14.25F);
+            this.label_Char.Location = new System.Drawing.Point(21, 24);
+            this.label_Char.Name = "label_Char";
+            this.label_Char.Size = new System.Drawing.Size(50, 22);
+            this.label_Char.TabIndex = 0;
+            this.label_Char.Text = "Char";
+            // 
+            // label_ASCII
+            // 
+            this.label_ASCII.AutoSize = true;
+            this.label_ASCII.Font = new System.Drawing.Font("Consolas", 14.25F);
+            this.label_ASCII.Location = new System.Drawing.Point(17, 76);
+            this.label_ASCII.Name = "label_ASCII";
+            this.label_ASCII.Size = new System.Drawing.Size(60, 22);
+            this.label_ASCII.TabIndex = 1;
+            this.label_ASCII.Text = "ASCII";
+            // 
+            // textBox_Char1
+            // 
+            this.textBox_Char1.BackColor = System.Drawing.Color.White;
+            this.textBox_Char1.Font = new System.Drawing.Font("Consolas", 14.25F);
+            this.textBox_Char1.Location = new System.Drawing.Point(83, 21);
+            this.textBox_Char1.Name = "textBox_Char1";
+            this.textBox_Char1.Size = new System.Drawing.Size(67, 30);
+            this.textBox_Char1.TabIndex = 2;
+            // 
+            // textBox_ASCII1
+            // 
+            this.textBox_ASCII1.BackColor = System.Drawing.Color.White;
+            this.textBox_ASCII1.Font = new System.Drawing.Font("Consolas", 14.25F);
+            this.textBox_ASCII1.Location = new System.Drawing.Point(83, 73);
+            this.textBox_ASCII1.Name = "textBox_ASCII1";
+            this.textBox_ASCII1.Size = new System.Drawing.Size(67, 30);
+            this.textBox_ASCII1.TabIndex = 3;
+            // 
+            // label_PS1
+            // 
+            this.label_PS1.AutoSize = true;
+            this.label_PS1.Font = new System.Drawing.Font("微软雅黑", 10F);
+            this.label_PS1.ForeColor = System.Drawing.Color.Red;
+            this.label_PS1.Location = new System.Drawing.Point(39, 148);
+            this.label_PS1.Name = "label_PS1";
+            this.label_PS1.Size = new System.Drawing.Size(184, 60);
+            this.label_PS1.TabIndex = 4;
+            this.label_PS1.Text = "键盘直接键入按键即可\r\n仅支持字母或数字转ASCII码\r\n字母自动同时转换大小写";
+            this.label_PS1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox_Char2
+            // 
+            this.textBox_Char2.BackColor = System.Drawing.Color.White;
+            this.textBox_Char2.Font = new System.Drawing.Font("Consolas", 14.25F);
+            this.textBox_Char2.Location = new System.Drawing.Point(156, 21);
+            this.textBox_Char2.Name = "textBox_Char2";
+            this.textBox_Char2.Size = new System.Drawing.Size(67, 30);
+            this.textBox_Char2.TabIndex = 5;
+            // 
+            // textBox_ASCII2
+            // 
+            this.textBox_ASCII2.BackColor = System.Drawing.Color.White;
+            this.textBox_ASCII2.Font = new System.Drawing.Font("Consolas", 14.25F);
+            this.textBox_ASCII2.Location = new System.Drawing.Point(156, 73);
+            this.textBox_ASCII2.Name = "textBox_ASCII2";
+            this.textBox_ASCII2.Size = new System.Drawing.Size(67, 30);
+            this.textBox_ASCII2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(247, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(11, 192);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(86, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 22);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "快捷转换";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(362, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 22);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "硬转换";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 10F);
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(300, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(184, 60);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "输入ASCII或字符后点击转换\r\n支持所有字符和ASCII互转\r\n多次使用请先点击清除";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox_ASCII3
+            // 
+            this.textBox_ASCII3.BackColor = System.Drawing.Color.White;
+            this.textBox_ASCII3.Font = new System.Drawing.Font("Consolas", 14.25F);
+            this.textBox_ASCII3.Location = new System.Drawing.Point(338, 73);
+            this.textBox_ASCII3.Name = "textBox_ASCII3";
+            this.textBox_ASCII3.Size = new System.Drawing.Size(107, 30);
+            this.textBox_ASCII3.TabIndex = 15;
+            // 
+            // textBox_Char3
+            // 
+            this.textBox_Char3.BackColor = System.Drawing.Color.White;
+            this.textBox_Char3.Font = new System.Drawing.Font("Consolas", 14.25F);
+            this.textBox_Char3.Location = new System.Drawing.Point(338, 21);
+            this.textBox_Char3.Name = "textBox_Char3";
+            this.textBox_Char3.Size = new System.Drawing.Size(107, 30);
+            this.textBox_Char3.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Consolas", 14.25F);
+            this.label6.Location = new System.Drawing.Point(272, 76);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 22);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "ASCII";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Consolas", 14.25F);
+            this.label7.Location = new System.Drawing.Point(276, 24);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 22);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Char";
+            // 
+            // button_Converter
+            // 
+            this.button_Converter.Font = new System.Drawing.Font("微软雅黑", 10F);
+            this.button_Converter.Location = new System.Drawing.Point(459, 21);
+            this.button_Converter.Name = "button_Converter";
+            this.button_Converter.Size = new System.Drawing.Size(59, 35);
+            this.button_Converter.TabIndex = 16;
+            this.button_Converter.Text = "转换";
+            this.button_Converter.UseVisualStyleBackColor = true;
+            this.button_Converter.Click += new System.EventHandler(this.Button_Converter_Click);
+            // 
+            // button_Clear
+            // 
+            this.button_Clear.Font = new System.Drawing.Font("微软雅黑", 10F);
+            this.button_Clear.Location = new System.Drawing.Point(459, 68);
+            this.button_Clear.Name = "button_Clear";
+            this.button_Clear.Size = new System.Drawing.Size(59, 35);
+            this.button_Clear.TabIndex = 17;
+            this.button_Clear.Text = "清除";
+            this.button_Clear.UseVisualStyleBackColor = true;
+            this.button_Clear.Click += new System.EventHandler(this.Button_Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 231);
+            this.Controls.Add(this.button_Clear);
+            this.Controls.Add(this.button_Converter);
+            this.Controls.Add(this.textBox_ASCII3);
+            this.Controls.Add(this.textBox_Char3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_ASCII2);
+            this.Controls.Add(this.textBox_Char2);
+            this.Controls.Add(this.label_PS1);
+            this.Controls.Add(this.textBox_ASCII1);
+            this.Controls.Add(this.textBox_Char1);
+            this.Controls.Add(this.label_ASCII);
+            this.Controls.Add(this.label_Char);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ASCII码转换器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_Char;
+        private System.Windows.Forms.Label label_ASCII;
+        private System.Windows.Forms.TextBox textBox_Char1;
+        private System.Windows.Forms.TextBox textBox_ASCII1;
+        private System.Windows.Forms.Label label_PS1;
+        private System.Windows.Forms.TextBox textBox_Char2;
+        private System.Windows.Forms.TextBox textBox_ASCII2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_ASCII3;
+        private System.Windows.Forms.TextBox textBox_Char3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button_Converter;
+        private System.Windows.Forms.Button button_Clear;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ASCII.Converter {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+
+            // 允许窗体预览键盘事件
+            this.KeyPreview = true;
+
+            // 绑定 KeyDown 事件
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+
+            // 修改只读
+            textBox_Char1.ReadOnly = true;
+            textBox_ASCII1.ReadOnly = true;
+            textBox_Char2.ReadOnly = true;
+            textBox_ASCII2.ReadOnly = true;
+
+            // 设置窗口为固定边框样式
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            // 禁用最大化按钮
+            this.MaximizeBox = false;
+
+            // 将最小尺寸和最大尺寸设置为当前尺寸
+            this.MinimumSize = this.Size;
+            this.MaximumSize = this.Size;
+
+        }
+
+        // 快捷转换 KeyDown 事件处理程序
+        private void Form1_KeyDown(object sender, KeyEventArgs e) {
+            
+            // 读取按键为 char
+            char ch = (char)e.KeyValue;
+
+            string s1, s2, a1, a2;
+
+            // 如果是字母
+            if(ch >='A' && ch <= 'z') {
+                textBox_Char1.BackColor = Color.White;
+                textBox_ASCII1.BackColor = Color.White;
+                textBox_Char2.BackColor = Color.White;
+                textBox_ASCII2.BackColor = Color.White;
+                s1 = ch.ToString();
+                s2 = (char.ToLower(ch)).ToString();
+                a1= ((int)ch).ToString();
+                a2= ((int)char.ToLower(ch)).ToString();
+                textBox_Char1.Text = s1;
+                textBox_Char2.Text = s2;
+                textBox_ASCII1.Text = a1;
+                textBox_ASCII2.Text = a2;
+            }
+
+            // 如果是数字
+            else if(ch >='0' && ch <= '9') {
+                textBox_Char1.BackColor = Color.White;
+                textBox_ASCII1.BackColor = Color.White;
+                textBox_Char2.BackColor = Form1.DefaultBackColor;
+                textBox_ASCII2.BackColor = Form1.DefaultBackColor;
+                textBox_Char2.Text = "-";
+                textBox_ASCII2.Text = "-";
+                s1 = ch.ToString();
+                a1 = ((int)ch).ToString();
+                textBox_Char1.Text = s1;
+                textBox_ASCII1.Text = a1;
+            }
+
+            else {
+                textBox_Char1.BackColor = Form1.DefaultBackColor;
+                textBox_ASCII1.BackColor = Form1.DefaultBackColor;
+                textBox_Char2.BackColor = Form1.DefaultBackColor;
+                textBox_ASCII2.BackColor = Form1.DefaultBackColor;
+                textBox_Char1.Text = "-";
+                textBox_ASCII1.Text = "-"; 
+                textBox_Char2.Text = "-";
+                textBox_ASCII2.Text = "-";
+            }
+            
+        }
+
+        private void Button_Converter_Click(object sender, EventArgs e) {
+            string s = "";
+
+            if (textBox_Char3.Text != "") {
+                textBox_ASCII3.Text = ((int)textBox_Char3.Text[0]).ToString();
+            }
+            
+            else if(textBox_ASCII3.Text != "") {
+                textBox_Char3.Text = ((char)Convert.ToInt32(textBox_ASCII3.Text)).ToString();
+            }
+        }
+
+        private void Button_Clear_Click(object sender, EventArgs e) {
+            textBox_Char3.Text = "";
+            textBox_ASCII3.Text = "";
+        }
+    }
+}
